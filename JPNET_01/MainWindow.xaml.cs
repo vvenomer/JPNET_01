df665b34bfd212719ba76da09d3752abab08019b
Blajer <pawel.krzysztof.blajer@intel.com>
2019-10-17 19:25:04 +0200

    init

---
+++ JPNET_01/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Xml;
+
+namespace JPNET_01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        XmlDocument doc;
+        public MainWindow()
+        {
+            doc = new XmlDocument();
+            doc.Load("mondial-3.0.xml");
+
+            InitializeComponent();
+
+            countryPopulation.Content = GetPopulation(countryName.Text);
+            countryList.ItemsSource = GetCountriesNames();
+        }
+
+        private void TextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                var textBox = (TextBox)sender;
+                countryPopulation.Content = GetPopulation(textBox.Text);
+            }
+        }
+        private void SelectedCountryInComboBox(object sender, SelectionChangedEventArgs e)
+        {
+            var countryName = ((ComboBox)sender).SelectedItem.ToString();
+            var country = GetCountry(countryName);
+            var code = country.Attributes["datacode"].Value;
+            var population = country.Attributes["population"].Value;
+            var totalArea = country.Attributes["total_area"].Value;
+            countryInfo.Document.Blocks.Clear();
+            countryInfo.Document.Blocks.Add(new Paragraph(new Run("Kod: " + code)));
+            countryInfo.Document.Blocks.Add(new Paragraph(new Run("Populacja: " + population)));
+            countryInfo.Document.Blocks.Add(new Paragraph(new Run("Powierzchnia: " + totalArea)));
+
+            country.GetElementsByName("province").ToList().ForEach(c =>
+            {
+                countryInfo.Document.Blocks.Add(new Paragraph(new Run("Miasto: " + c.GetElementsByName("name").First().Value)));
+            });
+        }
+
+        string GetPopulation(string text)
+        {
+            var node = GetCountry(text);
+            if (node == null)
+                return "Country not found";
+            return int.Parse(node.Attributes["population"].Value).ToString("0,0");
+        }
+
+        IEnumerable<XmlNode> GetCountries()
+        {
+            return doc.GetElementsByTagName("country").ToEnumerable();
+        }
+
+        XmlNode GetCountry(string name)
+        {
+            return GetCountries().GetByAttribute("name", name);
+        }
+
+        IEnumerable<string> GetCountriesNames()
+        {
+            return GetCountries().Select(c => c.Attributes["name"].Value);
+        }
+
+    }
+}
---
+++ JPNET_01/XmlExtentions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace JPNET_01
+{
+    static class XmlExtentions
+    {
+
+        public static XmlNode GetByAttribute(this IEnumerable<XmlNode> nodes, string attr, string value)
+        {
+            return nodes.FirstOrDefault(c => c.Attributes[attr].Value == value);
+        }
+
+        public static IEnumerable<XmlNode> ToEnumerable(this XmlNodeList list)
+        {
+            var nodes = new List<XmlNode>();
+
+            foreach (XmlNode node in list)
+            {
+                nodes.Add(node);
+            }
+            return nodes;
+        }
+
+        public static IEnumerable<XmlNode> GetElementsByName(this XmlNode nodes, string name)
+        {
+            return nodes.ChildNodes.ToEnumerable().Where(n => n.Name == name);
+        }
+    }
+}
